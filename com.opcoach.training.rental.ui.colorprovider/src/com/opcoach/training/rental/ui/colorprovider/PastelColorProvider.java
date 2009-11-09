ebffbeb48be2c1729584cb82af6b38a003a4faef
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2009-11-09 13:22:10 +0000

    initial
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@11 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ com.opcoach.training.rental.ui.colorprovider/src/com/opcoach/training/rental/ui/colorprovider/PastelColorProvider.java
@@ -0,0 +1,37 @@
+package com.opcoach.training.rental.ui.colorprovider;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
+import com.opcoach.training.rental.RentalObject;
+
+
+public class PastelColorProvider implements IColorProvider
+{
+
+	public PastelColorProvider()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Color getBackground(Object element)
+	{
+		// TODO Auto-generated method stub
+		return Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
+	}
+
+		public Color getForeground(Object element)
+		{		
+			int color = SWT.COLOR_BLACK;
+			if (element instanceof Customer) color = SWT.COLOR_DARK_BLUE;
+			else if (element instanceof Rental) color = SWT.COLOR_DARK_MAGENTA;
+			else if (element instanceof RentalObject) color = SWT.COLOR_DARK_GREEN;
+			
+			return  Display.getCurrent().getSystemColor(color);
+		}
+
+}
