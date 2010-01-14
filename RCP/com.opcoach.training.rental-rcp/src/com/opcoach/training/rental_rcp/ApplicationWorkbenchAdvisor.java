a557a3deaa09f536ee50e48149dba5b02ab39e4d
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2010-01-14 21:29:16 +0000

    The rental RCP Application
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@68 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ RCP/com.opcoach.training.rental-rcp/src/com/opcoach/training/rental_rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.opcoach.training.rental_rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.opcoach.training.rental_rcp.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
