2c8df111a071f474fc2e8f277cbc8a8147967c8d
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2010-04-25 11:44:19 +0000

    Gestion du menu contextuel sur l'arbre de l'agence
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@101 28ab6df1-7a97-4e50-b59a-519495a15fef

--- RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/views/RentalAgencyView.java
+++ RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/views/RentalAgencyView.java
@@ -1,11 +1,14 @@
 package com.opcoach.training.rental.ui.views;
 
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.part.ViewPart;
 
+import com.opcoach.training.rental.core.RentalCoreActivator;
 import com.opcoach.training.rental.ui.RentalUIActivator;
 
 public class RentalAgencyView extends ViewPart implements IPropertyChangeListener
@@ -23,9 +26,18 @@ public class RentalAgencyView extends ViewPart implements IPropertyChangeListene
 	public void createPartControl(Composite parent)
 	{
 		agencyViewer = new TreeViewer(parent);
-		agencyViewer.setContentProvider(new AgencyContentProvider(RentalUIActivator.getAgency()));
-		agencyViewer.setLabelProvider(new AgencyLabelProvider());
+		agencyViewer.setContentProvider(new AgencyContentProvider(RentalCoreActivator.getAgency()));
+		agencyViewer.setLabelProvider(new AgencyLabelProvider(RentalCoreActivator.getAgency()));
 		agencyViewer.setInput(AgencyContentProvider.ROOT_AGENCY_NODE);
+		
+		// Autorise le popup sur le treeviewer
+		MenuManager menuManager = new MenuManager();
+		Menu menu = menuManager.createContextMenu(agencyViewer.getControl());
+		agencyViewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuManager, agencyViewer);
+		
+		
+		
 		getSite().setSelectionProvider(agencyViewer);		
 		
 		//  On s'enregistre en tant que pref listener sur le preference store...
