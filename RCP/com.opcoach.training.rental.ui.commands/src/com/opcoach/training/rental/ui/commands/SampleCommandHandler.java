4f5917290b021382e68dd42561bc08a2367d69f6
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-06-05 21:20:55 +0000

    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@200 28ab6df1-7a97-4e50-b59a-519495a15fef

--- RCP/com.opcoach.training.rental.ui.commands/src/com/opcoach/training/rental/ui/commands/SampleCommandHandler.java
+++ RCP/com.opcoach.training.rental.ui.commands/src/com/opcoach/training/rental/ui/commands/SampleCommandHandler.java
@@ -2,8 +2,10 @@ package com.opcoach.training.rental.ui.commands;
 import java.util.Iterator;
 
 import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.State;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -28,7 +30,35 @@ public class SampleCommandHandler extends AbstractHandler
 				System.out.println("Objet selectionné : " + it.next());
 			}
 		}
+		
+		// Manage the state
+		Command c = event.getCommand();
+		for (String st : c.getStateIds())
+		{
+			System.out.println("Etat de la commande : " + st);
+			State s = c.getState(st);
+			System.out.println("Etat de : " + st + " value = " + s.getValue());
+
+			
+		}
+		State s = c.getState("com.opcoach.training.rental.ui.commands.state3");
+		s.setValue(false);
+		//HandlerUtil.updateRadioState(event.getCommand(), "Toto");
+		
+		
 		return null;
 	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#isEnabled()
+	 */
+	@Override
+	public boolean isEnabled()
+	{
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	
 
 }
