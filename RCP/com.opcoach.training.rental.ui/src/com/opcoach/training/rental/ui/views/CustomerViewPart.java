76e754d32fb07e9dad1c561f4cd5274f598d9a2d
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-06-05 21:19:30 +0000

    suppression des versions
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@198 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/views/CustomerViewPart.java
@@ -0,0 +1,117 @@
+package com.opcoach.training.rental.ui.views;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.FillLayout;
+
+public class CustomerViewPart extends ViewPart
+{
+
+	public static final String ID = "com.opcoach.training.rental.ui.views.CustomerViewPart"; //$NON-NLS-1$
+	private Text txtAName;
+	private Text txtUnPrnom;
+
+	public CustomerViewPart()
+	{
+	}
+
+	/**
+	 * Create contents of the view part.
+	 * @param parent
+	 */
+	@Override
+	public void createPartControl(Composite parent)
+	{
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new FormLayout());
+		
+		Group grpIdentity = new Group(container, SWT.NONE);
+		FormData fd_grpIdentity = new FormData();
+		fd_grpIdentity.bottom = new FormAttachment(0, 94);
+		fd_grpIdentity.right = new FormAttachment(0, 402);
+		fd_grpIdentity.top = new FormAttachment(0);
+		fd_grpIdentity.left = new FormAttachment(0);
+		grpIdentity.setLayoutData(fd_grpIdentity);
+		grpIdentity.setText("Identity");
+		grpIdentity.setLayout(new GridLayout(2, false));
+		
+		Label label = new Label(grpIdentity, SWT.NONE);
+		label.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 2, 1));
+		label.setText("display Name");
+		label.setAlignment(SWT.CENTER);
+		
+		Label lblNom = new Label(grpIdentity, SWT.NONE);
+		lblNom.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNom.setText("Nom : ");
+		
+		txtAName = new Text(grpIdentity, SWT.BORDER);
+		txtAName.setText("A name");
+		txtAName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblPrnom = new Label(grpIdentity, SWT.NONE);
+		lblPrnom.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblPrnom.setText("Pr\u00E9nom");
+		
+		txtUnPrnom = new Text(grpIdentity, SWT.BORDER);
+		txtUnPrnom.setText("Un pr\u00E9nom");
+		txtUnPrnom.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Group grpAdresse = new Group(container, SWT.NONE);
+		grpAdresse.setText("Adresse");
+		grpAdresse.setLayout(new FillLayout(SWT.HORIZONTAL));
+		FormData fd_grpAdresse = new FormData();
+		fd_grpAdresse.right = new FormAttachment(grpIdentity, 0, SWT.RIGHT);
+		fd_grpAdresse.bottom = new FormAttachment(100, -12);
+		fd_grpAdresse.top = new FormAttachment(grpIdentity, 6);
+		fd_grpAdresse.left = new FormAttachment(0);
+		grpAdresse.setLayoutData(fd_grpAdresse);
+		
+		Label lblAdresse = new Label(grpAdresse, SWT.NONE);
+		lblAdresse.setText("Adresse");
+
+		createActions();
+		initializeToolBar();
+		initializeMenu();
+	}
+
+	/**
+	 * Create the actions.
+	 */
+	private void createActions()
+	{
+		// Create the actions
+	}
+
+	/**
+	 * Initialize the toolbar.
+	 */
+	private void initializeToolBar()
+	{
+		IToolBarManager toolbarManager = getViewSite().getActionBars().getToolBarManager();
+	}
+
+	/**
+	 * Initialize the menu.
+	 */
+	private void initializeMenu()
+	{
+		IMenuManager menuManager = getViewSite().getActionBars().getMenuManager();
+	}
+
+	@Override
+	public void setFocus()
+	{
+		// Set the focus
+	}
+}
