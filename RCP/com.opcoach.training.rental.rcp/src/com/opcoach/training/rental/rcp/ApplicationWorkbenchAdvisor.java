b690ee2977792eecc64b753964e735b3a6603f9c
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-08-26 10:42:41 +0000

    Initial import.
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@243 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ RCP/com.opcoach.training.rental.rcp/src/com/opcoach/training/rental/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,26 @@
+// ------------------------------------------------
+// OPCoach Training Projects
+// © OPCoach 2010 http://www.opcoach.com
+// ------------------------------------------------
+
+package com.opcoach.training.rental.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.opcoach.training.rental.ui.views.RentalPerspective;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor
+{
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
+	{
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId()
+	{
+		return RentalPerspective.ID;
+	}
+}
