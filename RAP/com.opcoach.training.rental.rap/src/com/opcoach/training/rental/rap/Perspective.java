de516839f2abb3d4efdc093b7db1881bf871cf60
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-08-26 10:05:32 +0000

    Initial import.
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@227 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ RAP/com.opcoach.training.rental.rap/src/com/opcoach/training/rental/rap/Perspective.java
@@ -0,0 +1,20 @@
+package com.opcoach.training.rental.rap;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * Configures the perspective layout. This class is contributed through the
+ * plugin.xml.
+ */
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+
+		layout.addStandaloneView(View.ID, false, IPageLayout.LEFT, 1.0f,
+				editorArea);
+	}
+}
