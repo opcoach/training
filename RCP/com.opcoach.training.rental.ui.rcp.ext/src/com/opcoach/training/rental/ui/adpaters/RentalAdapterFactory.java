06a21a21b4e8b5c76f5c5f2d186360c147bc4ba4
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-08-26 10:27:15 +0000

    Extraction des accès spécifiques aux ressources, dans un fragment utilisable qu'en RCP. Permet de faire une migration sur RAP
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@231 28ab6df1-7a97-4e50-b59a-519495a15fef

---
+++ RCP/com.opcoach.training.rental.ui.rcp.ext/src/com/opcoach/training/rental/ui/adpaters/RentalAdapterFactory.java
@@ -0,0 +1,42 @@
+package com.opcoach.training.rental.ui.adpaters;
+
+import java.util.Date;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdapterFactory;
+
+import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
+import com.opcoach.training.rental.core.RentalCoreActivator;
+
+public class RentalAdapterFactory implements IAdapterFactory
+{
+
+	/** Transform an IFile into a Rental object if extension is .rental */
+	public Object getAdapter(Object adaptableObject, Class adapterType)
+	{
+		Rental result = null;
+		if ((adaptableObject instanceof IFile) && Rental.class.equals(adapterType))
+		{
+			IFile f = (IFile) adaptableObject;
+			if (f.getFileExtension().equals("rental"))
+			{
+				// Create a sample rental object... does not read the file...
+				RentalAgency agency = RentalCoreActivator.getAgency();
+				Customer c = agency.getCustomers().get(0);
+				RentalObject ro = agency.getObjectsToRent().get(0);
+				result = agency.book(c, ro, new Date(), new Date());
+			}
+		}
+		return result;
+	}
+
+	public Class[] getAdapterList()
+	{
+		// TODO Auto-generated method stub
+		return new Class[] { Rental.class };
+	}
+
+}
