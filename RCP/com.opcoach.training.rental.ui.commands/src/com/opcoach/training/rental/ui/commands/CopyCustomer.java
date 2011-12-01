a91d53b7fc6353bed6d1448c176c46417dbb1dc2
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2011-12-01 10:06:17 +0000

    Ajout du clipboard copy
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@258 28ab6df1-7a97-4e50-b59a-519495a15fef

--- RCP/com.opcoach.training.rental.ui.commands/src/com/opcoach/training/rental/ui/commands/CopyCustomer.java
+++ RCP/com.opcoach.training.rental.ui.commands/src/com/opcoach/training/rental/ui/commands/CopyCustomer.java
@@ -7,6 +7,11 @@ import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.RTFTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -25,10 +30,22 @@ public class CopyCustomer extends AbstractHandler implements IHandler
 			Object selectedObject = ((IStructuredSelection) currentSelection).getFirstElement();
 			if (selectedObject instanceof Customer)
 			{
+				Customer c = (Customer) selectedObject;
 				IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 				MessageDialog.openInformation(window.getShell(),
 						"Copy Client",
-						"On copie le client :" + ((Customer)selectedObject).getDisplayName());
+						"On copie le client :" + (c.getDisplayName()));
+				
+				
+				Clipboard clipboard = new Clipboard(Display.getCurrent());
+				String textData = c.getDisplayName();
+				String rtfData = "{\\rtf1\\b\\i " + textData + "}";
+				TextTransfer textTransfer = TextTransfer.getInstance();
+				RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+				Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer};
+				Object[] data = new Object[]{textData, rtfData};
+				clipboard.setContents(data, transfers);
+				clipboard.dispose();
 			}		
 
 		}
