f3d619ff18e404c01965f4f3c2caa55252bddb19
olivier <olivier@28ab6df1-7a97-4e50-b59a-519495a15fef>
2012-05-08 13:22:08 +0000

    Sample of color RGB access on a static color
    git-svn-id: svn+ssh://svn.opcoach.com/srv/d_opcoach/svn/training/trunk@324 28ab6df1-7a97-4e50-b59a-519495a15fef

--- RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/pref/RentalPrefInit.java
+++ RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/pref/RentalPrefInit.java
@@ -1,8 +1,12 @@
 package com.opcoach.training.rental.ui.pref;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.ui.RentalUIActivator;
 import com.opcoach.training.rental.ui.RentalUIConstants;
@@ -19,10 +23,15 @@ public class RentalPrefInit extends AbstractPreferenceInitializer implements Ren
 	public void initializeDefaultPreferences()
 	{
 		System.out.println("Bonjour");
-		RentalUIActivator.getDefault().getPreferenceStore().setDefault(CUSTOMER_KEY, StringConverter.asString(new RGB(0,0,255)));
-		RentalUIActivator.getDefault().getPreferenceStore().setDefault(RENTAL_KEY, StringConverter.asString(new RGB(255,0,0)));
-		RentalUIActivator.getDefault().getPreferenceStore().setDefault(RENTAL_OBJECT_KEY, StringConverter.asString(new RGB(0,255,0)));
-		RentalUIActivator.getDefault().getPreferenceStore().setDefault(DISPLAY_COUNT_PREF, StringConverter.asString(false));
+		IPreferenceStore ps = RentalUIActivator.getDefault().getPreferenceStore();
+		
+		Color c = Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+		
+		ps.setDefault(CUSTOMER_KEY, StringConverter.asString(c.getRGB()));
+		
+		ps.setDefault(RENTAL_KEY, StringConverter.asString(new RGB(255,0,0)));
+		ps.setDefault(RENTAL_OBJECT_KEY, StringConverter.asString(new RGB(0,255,0)));
+		ps.setDefault(DISPLAY_COUNT_PREF, StringConverter.asString(false));
 		
 		
 	}
