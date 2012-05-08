17c368723c1aa5661dd6a41110b42cf4427383bc
Olivier Prouvost <olivier.prouvost@opcoach.com>
2012-05-08 18:39:40 +0200

    test commit

--- RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/RentalUIActivator.java
+++ RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/RentalUIActivator.java
@@ -40,6 +40,7 @@ public class RentalUIActivator extends AbstractUIPlugin implements  RentalUICons
 	 */
 	public RentalUIActivator()
 	{
+		System.out.println("Start Rental UI Activator");
 	}
 
 	/*
