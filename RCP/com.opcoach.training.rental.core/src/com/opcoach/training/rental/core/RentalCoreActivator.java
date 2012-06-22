eb64dca7ae616a2030ff0932b06b3c7a23421a3d
Olivier Prouvost <olivier.prouvost@opcoach.com>
2012-06-22 16:35:20 +0200

    A problem with the jar file...

--- RCP/com.opcoach.training.rental.core/src/com/opcoach/training/rental/core/RentalCoreActivator.java
+++ RCP/com.opcoach.training.rental.core/src/com/opcoach/training/rental/core/RentalCoreActivator.java
@@ -1,59 +1,41 @@
 package com.opcoach.training.rental.core;
 
-import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import com.opcoach.training.rental.MyRentalAgency;
-import com.opcoach.training.rental.core.helpers.RentalAgencyGenerator;
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.helpers.RentalAgencyGenerator;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class RentalCoreActivator extends Plugin {
+public class RentalCoreActivator implements BundleActivator {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.opcoach.training.rental.core";
 
-	// The shared instance
-	private static RentalCoreActivator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public RentalCoreActivator() {
-	}
-
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
+
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static RentalCoreActivator getDefault() {
-		return plugin;
-	}
-	
-	private static MyRentalAgency agency = RentalAgencyGenerator.createSampleAgency();
-	
-	public static MyRentalAgency getAgency()
-	{		
+	private static RentalAgency agency = RentalAgencyGenerator.createSampleAgency();
+
+	public static RentalAgency getAgency() {
 		return agency;
 	}
 
