d845d86d6c3e95b62315058cfbe56fa1d1a5e343
Olivier Prouvost <olivier.prouvost@opcoach.com>
2013-10-11 10:41:35 +0200

    Comment the DIViewPart and remove dependency to tools.compat… Not sure this is a good way for migration.

--- CodeSamples/com.opcoach.training.e4.codesamples/src/com/opcoach/training/e4/codesamples/diviewpart/ViewAsViewPart.java
+++ CodeSamples/com.opcoach.training.e4.codesamples/src/com/opcoach/training/e4/codesamples/diviewpart/ViewAsViewPart.java
@@ -1,11 +1,11 @@
 package com.opcoach.training.e4.codesamples.diviewpart;
 
-import org.eclipse.e4.tools.compat.parts.DIViewPart;
+//import org.eclipse.e4.tools.compat.parts.DIViewPart;
 
-public class ViewAsViewPart extends DIViewPart<ViewAsPojo>{
+public class ViewAsViewPart {} /* extends DIViewPart<ViewAsPojo>{
 
 	public ViewAsViewPart() {
 		super(ViewAsPojo.class);
 	}
 
-}
+} */
