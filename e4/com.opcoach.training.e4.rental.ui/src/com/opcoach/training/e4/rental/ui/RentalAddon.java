c51618d3bf4e581f1d7bdbab79675397f156d05e
Olivier Prouvost <olivier.prouvost@opcoach.com>
2013-10-23 14:35:37 +0200

    Actually the paletteManager must be injected in context (it is used by the preference color page ) !

--- e4/com.opcoach.training.e4.rental.ui/src/com/opcoach/training/e4/rental/ui/RentalAddon.java
+++ e4/com.opcoach.training.e4.rental.ui/src/com/opcoach/training/e4/rental/ui/RentalAddon.java
@@ -46,7 +46,7 @@ public class RentalAddon implements RentalUIConstants
 
 		// Read the palettes extensions and publish it in context
 		readPaletteExtensions(reg);
-		// The palette manager is useless in context (use diretly the palette)
+		ctx.set(PALETTE_MANAGER, paletteManager);
 
 		// Set the current palette in context...
 		String palId = prefStore.getString(PREF_PALETTE);
