693877610b937916d1320d1dea1687b21d5ee9db
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-02-02 18:50:11 +0100

    Migrate the old E3 code to a pure E4 plugin.
    Change-Id: Ia342d63498756cbad7e6bf11acc115575c7090fd
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- M2T/com.opcoach.training.rental.acceleo.ui/src/com/opcoach/training/rental/acceleo/ui/handlers/GenerateHtmlHandler.java
+++ M2T/com.opcoach.training.rental.acceleo.ui/src/com/opcoach/training/rental/acceleo/ui/handlers/GenerateHtmlHandler.java
@@ -4,60 +4,46 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.emf.common.util.BasicMonitor;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.acceleo.main.GenerateHtml;
 
 /**
- * Our sample handler extends AbstractHandler, an IHandler base class.
- * 
- * @see org.eclipse.core.commands.IHandler
- * @see org.eclipse.core.commands.AbstractHandler
+ * E4 Handler code to call the acceleo generator
  */
-public class GenerateHtmlHandler extends AbstractHandler
+public class GenerateHtmlHandler
 {
-	/**
-	 * The constructor.
-	 */
-	public GenerateHtmlHandler()
+	/** This method is called by the framework which inject the expected values
+	 * @param isel : the current selection received as an IStructuredSelection (E3 side) */
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection isel) 
 	{
-	}
-
-	/**
-	 * the command has been executed, so extract extract the needed information
-	 * from the application context.
-	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException
-	{
-
-		ISelection sel = HandlerUtil.getCurrentSelection(event);
-		if (sel instanceof IStructuredSelection)
-		{
-			IStructuredSelection isel = (IStructuredSelection) sel;
 			Object selected = isel.getFirstElement();
 			if (selected instanceof RentalAgency)
-			{
-				RentalAgency a = (RentalAgency) selected;
-
-				File targetFolder = new File("/tmp");
-				try
-				{
-					GenerateHtml gen = new GenerateHtml(a, targetFolder, Collections.EMPTY_LIST);
-					gen.doGenerate(new BasicMonitor());
-				} catch (IOException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+				executeForAgency((RentalAgency) selected);	
+	}
+	
+	/** This method is called locally or by the framework which inject the expected values
+	 * @param a : the current selection  received as an RentalAgency (E4 side) */
+	@Execute
+	public void executeForAgency(@Named(IServiceConstants.ACTIVE_SELECTION) RentalAgency a) 
+	{
+		File targetFolder = new File("/tmp");
+		try
+		{
+			GenerateHtml gen = new GenerateHtml(a, targetFolder, Collections.emptyList());
+			gen.doGenerate(new BasicMonitor());
+			System.out.println("File generated in : /tmp");
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return null;
 	}
 }
