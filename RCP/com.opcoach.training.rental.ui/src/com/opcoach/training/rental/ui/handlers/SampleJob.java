10bac0385ce6e65220cf4b8916eee521af18d3ea
Olivier Prouvost <olivier.prouvost@opcoach.com>
2018-10-21 20:08:17 +0200

    Update pom, target platform, test launcher in training
    Change-Id: I177dc0fcd23cca5a9d8cd4298c677963d603e5cb
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/handlers/SampleJob.java
+++ RCP/com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/handlers/SampleJob.java
@@ -3,31 +3,34 @@ package com.opcoach.training.rental.ui.handlers;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 
 public class SampleJob extends Job
-{
-	public SampleJob()
-	{
-		super("Computation Job");
-	}
+{	
+	public SampleJob()  	{  super("Computation Job");  	}
 
-	@Override
+	@Override 
 	protected IStatus run(IProgressMonitor monitor)
 	{
+		SubMonitor progress = SubMonitor.convert(monitor, 10000);
+		
 		// Compute 10000 steps
-		monitor.beginTask("Compute", 10000);
+		progress.beginTask("Compute", 10000);
 		
 		for (int i = 0; i < 10000; i++)
 		{
-			// Do Something...
+			progress.setTaskName("Computing the " + i + " th block");
+
 			for (int j = 0; j < 10000; j++)
 			{
-				double acos = Math.cos((double) j);
+				 Math.cos((double) j);
 			}
-			
 			// One step done
-			monitor.worked(1);
+			progress.worked(1);
+			
+			if (progress.isCanceled()) 
+				return Status.CANCEL_STATUS;
 		}
 
 		return Status.OK_STATUS;
