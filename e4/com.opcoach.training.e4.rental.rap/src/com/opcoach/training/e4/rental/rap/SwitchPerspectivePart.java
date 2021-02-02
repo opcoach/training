255c5cec49911e2d389af87a6d660f5b816fd5da
Olivier Prouvost <olivier.prouvost@opcoach.com>
2021-02-02 22:56:49 +0100

    Update the switch Perspective with the new target platform
    (Use the same code than for pure E4 application).
    
    Import javax.annotation in package dependency
    
    Change-Id: I29cf9a5116edb8908cb426aba2ad6f091f61d189
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- e4/com.opcoach.training.e4.rental.rap/src/com/opcoach/training/e4/rental/rap/SwitchPerspectivePart.java
+++ e4/com.opcoach.training.e4.rental.rap/src/com/opcoach/training/e4/rental/rap/SwitchPerspectivePart.java
@@ -1,16 +1,18 @@
 
 package com.opcoach.training.e4.rental.rap;
 
-import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspectiveStack;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.swt.widgets.Composite;
+import org.osgi.service.event.Event;
 
 import com.opcoach.training.e4.rental.ui.RentalUIConstants;
 
@@ -23,24 +25,21 @@ public class SwitchPerspectivePart
 
 	private static final String PERS_STACK = "com.opcoach.training.e4.rental.rap.perspectiveStack";
 
-	/** The postConstruct does not create any widget. It only switch to expected perspective */
-	@PostConstruct
-	public void switchToStartPerspective(Composite parent, EPartService ps, MPart part, 
-			                  EModelService ms, MApplication appli)
+	/** Must react on application start up complete to switch the perspective (it does not work (anymore) in the PostConstruct))...  */
+	@Inject
+	@Optional
+	public void switchToStartPerspective(@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, EPartService ps, EModelService ms, MApplication appli,
+			MWindow refWin)
 	{
-		// get current window of current MPart
-		MWindow refWin = ms.getTopLevelWindowFor(part);
-		
-		// Suppose perspective is in a snippet. Must clone it 
-		MPerspective p = (MPerspective) ms.cloneSnippet(appli, RentalUIConstants.RENTAL_UI_PERSPECTIVE, refWin);
+		MPerspective mainPerspective = (MPerspective) ms.cloneSnippet(appli, RentalUIConstants.RENTAL_UI_PERSPECTIVE, refWin);
 
-		// Add this perspective in  the perspective Stack 
+		// Add this perspective in the perspective Stack
 		MPerspectiveStack pstack = (MPerspectiveStack) ms.find(PERS_STACK, appli);
-		pstack.getChildren().add(p);
+		pstack.getChildren().add(mainPerspective);
 
-		// Can now switch to this perspectives
-		ps.switchPerspective(p);
-
+		// Can now switch to this perspective
+		ps.switchPerspective(mainPerspective);
 	}
+
 
 }
